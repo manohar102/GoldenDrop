e495f0fd30e106e5ea5ba2fe322afd7659c97000
manohar krishna <manoharkrishna102@gmail.com>
2024-01-27 12:50:14 +0530

    First Commit

---
+++ src/main/java/com/techbuddy/goldendrop/GoldendropApplication.java
@@ -0,0 +1,13 @@
+package com.techbuddy.goldendrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldendropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldendropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techbuddy/goldendrop/GoldendropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbuddy.goldendrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldendropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
