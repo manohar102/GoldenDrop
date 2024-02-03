b813799bed044641e06ea3c11fa7382ac51081b2
sai kumar kollati <lskumarkollati721@gmail.com>
2024-02-03 16:13:48 +0530

    Added HttpMessageNotReadableException handling for Enums

--- src/main/java/com/techbuddy/goldendrop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/techbuddy/goldendrop/exception/GlobalExceptionHandler.java
@@ -5,14 +5,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = InvalidStoreException.class)
@@ -52,5 +56,58 @@ public class GlobalExceptionHandler {
         List<String> templateMessages = new ArrayList<>();
         exception.getConstraintViolations().forEach(violation -> templateMessages.add(violation.getMessageTemplate()));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(templateMessages);
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    public ResponseEntity nonReadableException(HttpMessageNotReadableException exception) {
+        String exceptionMessage = exception.getMessage();
+        if (StringUtils.hasText(exceptionMessage) && isEnumValidationError(exceptionMessage)) {
+            return buildEnumValidationErrorResponse(exceptionMessage);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionMessage);
+    }
+
+    private ResponseEntity buildEnumValidationErrorResponse(String exceptionMessage) {
+        String enumClass = getEnumClassFromErrorMessage(exceptionMessage);
+        String validEnumValues = getValidEnumValuesFromErrorMessage(exceptionMessage);
+        log.error("Not a valid value for enum {} . Valid validEnumValues are {} ", enumClass, validEnumValues);
+        String errorMessage =
+                String.format("Invalid enum value for %s. Valid validEnumValues are %s ", enumClass, validEnumValues);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    }
+
+    private String getEnumClassFromErrorMessage(String exceptionMessage) {
+        int firstIndex = exceptionMessage.indexOf("`");
+        int lastIndex = exceptionMessage.lastIndexOf("`");
+        if (firstIndex == -1 || lastIndex == -1) {
+            return "";
+        }
+        String enumClassWithPackageName = exceptionMessage.substring(firstIndex + 1, lastIndex);
+
+        return StringUtils.hasText(enumClassWithPackageName)
+                ? getEnumClassNameWithoutPackageName(enumClassWithPackageName)
+                : "";
+    }
+
+    private String getEnumClassNameWithoutPackageName(String enumClassWithPackageName) {
+        int lastIndexOf = enumClassWithPackageName.lastIndexOf(".");
+        if (lastIndexOf == -1) {
+            return "";
+        }
+        return enumClassWithPackageName.substring(lastIndexOf + 1);
+    }
+
+    private String getValidEnumValuesFromErrorMessage(String exceptionMessage) {
+        int indexOfOpeningBracket = exceptionMessage.indexOf("[");
+        int indexOfClosingBracket = exceptionMessage.indexOf("]");
+
+        if (indexOfOpeningBracket == -1 || indexOfClosingBracket == -1) {
+            return "";
+        }
+        return exceptionMessage.substring(indexOfOpeningBracket + 1, indexOfClosingBracket);
+    }
+
+    private boolean isEnumValidationError(String exceptionMessage) {
+        return exceptionMessage.contains("not one of the values accepted for Enum class");
     }
 }
