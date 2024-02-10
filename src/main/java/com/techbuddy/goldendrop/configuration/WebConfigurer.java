de02b5e9521ec18884dcef088d687062c09c90c8
manohar krishna <manoharkrishna102@gmail.com>
2024-02-10 18:50:44 +0530

    Basic Specifications added

---
+++ src/main/java/com/techbuddy/goldendrop/configuration/WebConfigurer.java
@@ -0,0 +1,33 @@
+package com.techbuddy.goldendrop.configuration;
+
+import com.example.eshow.annotation.SpecificationHandlerMethodArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new SpecificationHandlerMethodArgumentResolver());
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("PUT", "DELETE","POST","GET")
+                //   .allowedOrigins("*")
+                .exposedHeaders("Authorization");
+    }
+}
